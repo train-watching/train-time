3e9e020ead8fdf5028db1e8ab1b375eb412eb16c
Nathan Kelber <nkelber@gmail.com>
2020-08-08 16:42:25 -0400

    "First commit
    Updated intermediate-python-1.ipynb"

---
+++ intermediate-python-1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <img align="left" src="https://ithaka-labs.s3.amazonaws.com/static-files/images/tdm/tdmdocs/CC_BY.png"><br />
+# 
+# Created by [Nathan Kelber](http://nkelber.com) and Ted Lawless for [JSTOR Labs](https://labs.jstor.org/) under [Creative Commons CC BY License](https://creativecommons.org/licenses/by/4.0/)<br />
+# For questions/comments/improvements, email nathan.kelber@ithaka.org.<br />
+# ___
+
+# # Intermediate Python I
+# 
+# **Description:** This notebook describes how to:
+# * Reading and writing files (.txt, .csv, .json)
+# * Modifying a 
+# 
+# This is part 1 of 3 in the series Intermediate Python that will prepare you to do text analysis using the Python programming language.
+# 
+# **Difficulty:** Intermediate
+# 
+# **Knowledge Required:** 
+# * [Getting Started with Jupyter Notebooks](../../0-beginner/getting-started-with-jupyter.ipynb)
+# * [Python Basics I](../../0-beginner/python-basic-skills/python-basics-1.ipynb)
+# * [Python Basics II](../../0-beginner/python-basic-skills/python-basics-2.ipynb)
+# * [Python Basics III](../../0-beginner/python-basic-skills/python-basics-3.ipynb)
+# 
+# **Knowledge Recommended:** None
+# 
+# **Completion Time:** 75 minutes
+# 
+# **Data Format:** None
+# 
+# **Libraries Used:** Pandas
+# ___
+
+# In[ ]:
+
+
+# Small change
+
